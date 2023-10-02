0f007c14145a433fe6dbdb67abdf2216fde2a937
Mizan <mizanali097@gmail.com>
2023-10-03 00:37:04 +0530

    Manage-Members

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -55,7 +55,9 @@ export const ServerHeader = ({
             )}
 
             {isAdmin && (
-                <DropdownMenuItem className=" px-3 py-2 text-sm cursor-pointer" >
+                <DropdownMenuItem 
+                onClick={() => onOpen("members",{server})}
+                className=" px-3 py-2 text-sm cursor-pointer" >
                     Manage Members
                     <User className="h-4 w-4 ml-auto" />
                 </DropdownMenuItem>
