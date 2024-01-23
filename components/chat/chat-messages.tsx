cd4a57035d9bd599db4b17f71581fd4370c09948
Mizan <mizanali097@gmail.com>
2024-01-23 22:22:14 +0530

    realime

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment, useEffect, useRef } from "react";
 import ChatItem from "./chat-item";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
@@ -36,12 +37,17 @@ export const ChatMessages = ({name,member,chatId,apiUrl,socketUrl,socketQuery,pa
         const messageEndRef = useRef(null);
 
         const queryKey = `chat:${chatId}`;
+        const addKey = `chat:${chatId}:messages`;
+        const updateKey = `chat:${chatId}:messages:update`;
+
         const {data ,fetchNextPage,hasNextPage,isFetchingNextPage, status} = useChatQuery({
             queryKey,
             apiUrl,
             paramKey,
             paramValue
         });
+
+        useChatSocket({queryKey,addKey,updateKey});
 
 
         useEffect(() => {
--- pages/api/socket/messages/index.ts
+++ pages/api/socket/messages/index.ts
@@ -78,7 +78,7 @@ export default async function handler (
             }
         });
 
-        const channelKey = `chat:${channelId}:message`;
+        const channelKey = `chat:${channelId}:messages`;
         res?.socket?.server?.io.emit(channelKey,message);
         return res.status(200).json(message);
 
